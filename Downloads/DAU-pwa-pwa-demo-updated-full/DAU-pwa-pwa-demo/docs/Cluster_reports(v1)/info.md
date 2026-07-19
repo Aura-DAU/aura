c1ed83cbd37b832669a9a204fc2a82f4de514ae2
Parth Agrawal <parthagrawal2904@gmail.com>
2026-07-19 09:34:55 +0530

    Delete Downloads/DAU-pwa-pwa-demo-updated-full directory

--- Downloads/DAU-pwa-pwa-demo-updated-full/DAU-pwa-pwa-demo/docs/Cluster_reports(v1)/info.md
+++
@@ -1,1 +0,0 @@
-Here we have all the information about the clusters , its coverage and knowledge gaps in version 1
--- Downloads/DAU-pwa-pwa-demo-updated-full/DAU-pwa-pwa-demo/server/db/migrations/003_latency_logs.sql
+++
@@ -1,9 +0,0 @@
--- Migration: Create latency_logs table
-CREATE TABLE latency_logs (
-    id SERIAL PRIMARY KEY,
-    guardrail_time DOUBLE PRECISION NOT NULL,
-    retrieval_time DOUBLE PRECISION NOT NULL,
-    generation_time DOUBLE PRECISION NOT NULL,
-    total_time DOUBLE PRECISION NOT NULL,
-    created_at TIMESTAMPTZ DEFAULT now()
-);
